df37a4eb44bdcf4c42a1eafd658cf19e4ad2eb79
franCordobaDeveloper <franCordobaDeveloper>
2025-09-22 18:35:33 -0300

    Initial commit

---
+++ src/admin/loyouts/AdminLayout.tsx
@@ -0,0 +1,11 @@
+
+import { Outlet } from 'react-router'
+
+export const AdminLayout = () => {
+    return (
+        <div className='bg-blue-600'>
+
+            <Outlet />
+        </div>
+    )
+}
---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,14 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
+
+export const SearchPage = () => {
+    return (
+        <>
+            {/* Header */}
+            <CustomJumbotron title="Superhero Search" description="Discover, explore, and manage your favorite superheroes and villains" />
+        </>
+    )
+}
+
+
+export default SearchPage;
